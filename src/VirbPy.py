587828b7e038c758304ac9c49eb0795349b5f45e
S. Co1 <sco1.git@gmail.com>
2021-05-28 12:45:13 -0400

    Swap Typer in for Click

--- src/VirbPy.py
+++ src/VirbPy.py
@@ -1,13 +1,13 @@
 import json
 import subprocess
 import tkinter as tk
+import typing as t
 from datetime import datetime as dt
 from pathlib import Path
 from tkinter import filedialog
-from typing import List, Union
 
-import click
 import pandas as pd
+import typer
 from dateutil.parser import parse
 
 
@@ -34,8 +34,10 @@ FIELDS_TO_EXPORT = {  # -d flag
     "ePositionUncertainty_4": "Position Uncertainty",
 }
 
+virbpy_cli = typer.Typer()
 
-def processing_pipeline(datadir: Path) -> None:
+
+def processing_pipeline(data_dir: Path) -> None:
     """
     Recursively search for *.fit files contained in the provided directory & convert to *.xlsx.
 
@@ -43,9 +45,12 @@ def processing_pipeline(datadir: Path) -> None:
 
     Note: *.fit files with an exactly named *.json partner in the same directory are ignored.
     """
+    if data_dir is None:
+        raise ValueError("No processing directory specified")
+
     # Convert unconverted files to JSON & add to XLSX conversion queue
     excel_conversion_queue = []
-    for fit_file in datadir.rglob("*.fit"):
+    for fit_file in data_dir.rglob("*.fit"):
         # Check for existing conversion
         file_as_json = fit_file.with_suffix(".json")
         if file_as_json.exists():
@@ -61,9 +66,9 @@ def processing_pipeline(datadir: Path) -> None:
 
 def build_cli_cmd(
     in_filepath: Path,
-    out_filepath: Union[Path, None] = None,
+    out_filepath: t.Union[Path, None] = None,
     sort_order: str = SORT_ORDER,
-    fields_to_export: List = FIELDS_TO_EXPORT.keys(),
+    fields_to_export: t.Iterable[str] = FIELDS_TO_EXPORT.keys(),
     converter_path: Path = PATH_TO_CONVERTER,
 ) -> str:
     """Build the CLI command string from the provided parameters to pass to GMetrixConverter."""
@@ -127,31 +132,41 @@ def fit_json_to_excel(in_filepath: Path) -> None:
 
 
 def _time_since_start(timestamp: str, start_time: dt) -> float:
-    timestamp = parse(timestamp)
-    delta = timestamp - start_time
+    timestamp_dt = parse(timestamp)
+    delta = timestamp_dt - start_time
     return delta.total_seconds()
 
 
-@click.command()
-@click.option("-d", "--datadir", default=None, help="Top level data directory")
-def cli(datadir: Union[str, None]) -> None:
-    """
-    CLI Userflow.
+def _prompt_for_dir(start_dir: Path = Path()) -> Path:  # pragma: no cover
+    """Open a Tk file selection dialog to prompt the user to select a directory for processing."""
+    root = tk.Tk()
+    root.withdraw()
 
-    If no `datadir` is explicitly specified, the user is prompted to select the
-    top-level data directory.
-    """
-    if not datadir:
-        # Generate a Tk file selection dialog to select the top level data dir
-        # if none is provided on the CLI
-        root = tk.Tk()
-        root.withdraw()
-        datadir = Path(filedialog.askdirectory(title="Select Data Directory"))
-    else:
-        datadir = Path(datadir)
+    return Path(
+        filedialog.askdirectory(
+            title="Select directory for batch processing",
+            initialdir=start_dir,
+        )
+    )
 
-    processing_pipeline(datadir)
+
+@virbpy_cli.command()
+def batch(
+    data_dir: Path = typer.Option(None, exists=True, file_okay=False, dir_okay=True),
+) -> None:
+    """ """
+    if data_dir is None:
+        data_dir = _prompt_for_dir()
+
+    processing_pipeline(data_dir)
+
+
+@virbpy_cli.callback(invoke_without_command=True, no_args_is_help=True)
+def main(ctx: typer.Context) -> None:  # pragma: no cover
+    """ """
+    # Provide a callback for the base invocation to display the help text & exit.
+    pass
 
 
 if __name__ == "__main__":
-    cli()
+    virbpy_cli()
